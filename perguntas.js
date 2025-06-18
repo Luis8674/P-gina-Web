19f2e03b3ce3219e0b97f5e323ac937a353126b2
Luis8674 <luis.lorente.marinho@escola.pr.gov.br>
2025-06-18 14:27:00 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Titanfall 2',
+    'Qual o nome do robô protagonista?',
+    'BT 7274'
+)
+
+criaCartao(
+    'Hollow Knight Silksong',
+    'Qual o nome do protagonista do Hollow Knight Silksong?',
+    'Hornet'
+)
+
+criaCartao(
+    'Star Wars',
+    'Quem é o Darth Vader?',
+    'Anakin Skywalker'
+)
+
+criaCartao(
+    'Cyberpunk 2077',
+    'Qual o nome dos protagonistas do Cyberpunk 2077?',
+    'V e Johnny Silverhand'
+)
+
+criaCartao(
+    'Devil May Cry',
+    'Quem é o irmão de Dante?',
+    'Vergil'
+)
+
+criaCartao(
+    'Hallowen',
+    'Quem é o antagonista da franquia de filme Hallowen?',
+    'Michael Myers'
+)
